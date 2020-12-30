5c7d4c8f0b00413d7f72e5b85d2821e1360e098d
skyme32 <marcosmejiasg@gmail.com>
2020-12-30 14:29:59 +0100

    Add animations

--- app/src/main/java/com/udacity/asteroidradar/MainActivity.kt
+++ app/src/main/java/com/udacity/asteroidradar/MainActivity.kt
@@ -2,11 +2,28 @@ package com.udacity.asteroidradar
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this, navController, AppBarConfiguration(setOf(R.id.mainFragment)))
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return this.findNavController(R.id.nav_host_fragment).navigateUp()
     }
 }
